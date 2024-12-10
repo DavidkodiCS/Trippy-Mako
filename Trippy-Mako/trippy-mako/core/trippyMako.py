d4cda1641bcb8d02bcf62d642df95fdafe86e360
DavidkodiCS <m253312@usna.edu>
2024-12-10 15:26:15 -0500

    working on cli continued

--- Trippy-Mako/trippy-mako/core/trippyMako.py
+++ Trippy-Mako/trippy-mako/core/trippyMako.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
-while True:
-    try:
-        with open("C:\Users\dhkre\Desktop\Capstone\Trippy-Mako\Trippy-Mako\trippy-mako\core\welcome\w1.txt", "r") as f:
-            print(f.read())
+import click
+
+@click.command()
+    
+def trippyMako():
+    while True:
+        try:
+            with open("w1.txt", "r") as f:
+                print(f.read())
+        except FileNotFoundError:
+            print("Error: Welcome file not found...")
             
-    except FileNotFoundError:
-        print("Error: Welcome file not found...")
-    except Exception as e:
-        print("Error: Unexpected error occurred, {e}")
-        raise
+        t = input()
+        print(t)
+
+if __name__ == '__main__':
+    trippyMako()
