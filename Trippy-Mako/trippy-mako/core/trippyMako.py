2a5918ab6a65541f27811275f87748db3ebd6b9e
DavidkodiCS <m253312@usna.edu>
2024-12-10 19:29:40 -0500

    added help command and exit command

--- Trippy-Mako/trippy-mako/core/trippyMako.py
+++ Trippy-Mako/trippy-mako/core/trippyMako.py
@@ -1,18 +1,37 @@
-#!/usr/bin/env python
-import click
+#!/usr/bin/env python3
 
-@click.command()
-    
-def trippyMako():
-    while True:
+##Command Functions
+def help():
+    try:
+        with open("help.txt", "r") as f:
+            print(f.read())
+    except FileNotFoundError:
+        print("Error: Welcome file not found...")  
+
+
+##Main Functions
+def welcome():
         try:
             with open("w1.txt", "r") as f:
                 print(f.read())
         except FileNotFoundError:
-            print("Error: Welcome file not found...")
+            print("Error: Welcome file not found...")    
+        print("Type help to see the available commands or exit to quit...\n\n")
+    
+def trippyMako():
+    while True:
+        command = input("\n> ").strip()
+        
+        if command == 'exit':
+            print("Exiting...")
+            break
+        elif command == 'help':
+            help()
             
-        t = input()
-        print(t)
+        
 
+
+## MAIN ##
 if __name__ == '__main__':
+    welcome()
     trippyMako()
