b42ba7e1b80cb854f345903808f1d99ceeb8021b
DavidkodiCS <m253312@usna.edu>
2024-12-11 19:31:05 -0500

    AddConfiguration still does not add to the file after program is restarted.
    Edit configuration not complete.
    
    Some bugs.

--- Trippy-Mako/trippy-mako/core/trippyMako.py
+++ Trippy-Mako/trippy-mako/core/trippyMako.py
@@ -1,101 +1,153 @@
 #!/usr/bin/env python3
 
-#Imports
+## Imports ##
 import configparser
 import turnTM
 
-##Command Functions
+# User Configurations
+configuration = configparser.ConfigParser()
+configuration.read("configs.ini")
+
+## Command Functions ##
 def help():
     try:
         with open("help.txt", "r") as f:
             print(f.read())
     except FileNotFoundError:
-        print("Error: Welcome file not found...")  
+        print("Error: Welcome file not found...")
+
 
 def config():
+    print("Configuration Menu")
     configOptions()
-    
+
     while True:
         command = input("\nconfig > ").strip()
-        
+
         match command:
-            case 'exit':
+            case "exit":
                 print("Returning to Home...")
-                break             
-            case 'create':
+                break
+            case "create":
                 addConfig()
-            case 'remove':
+            case "remove":
                 removeConfig()
-            case 'edit':
+            case "edit":
                 editConfig()
-            case 'list':
+            case "list":
                 listConfig()
-            case 'options':
+            case "options":
                 configOptions()
+            case "display":
+                displayConfig()
             case _:
                 print("Unrecognized Command...")
+
 
 def connect():
     pass
 
+
 def sendPayload():
     pass
+
 
 def proxy():
     pass
 
-#Configuration Commands
+
+## Configuration Commands ##
 def addConfig():
     name = input("Enter configuration name: ")
+    
+    while configuration.has_section(name):
+        print("Configuration name already exists.\nWould you like to 'edit' the existing config or 'create' a new one?")
+        command = input("config > ")
+        if(command == 'edit'):
+            editConfig()
+            return
+        elif command == 'create':
+            name = input("Enter configuration name: ")
+            
     turnIP = input("Enter TURN server IP address: ")
     turnPort = input("Enter TURN server port number: ")
     protocol = input("Enter desired protocol: ")
+            
+    configuration.add_section(name)
+    configuration[name]['turnIP'] = turnIP
+    configuration[name]['turnPort'] = turnPort
+    configuration[name]['protocol'] = protocol
     
-def removeConfig():
-    pass
+    print("Configuration successfully added!")
+        
 
+
+def removeConfig():
+    listConfig()
+    remove = input("Choose section to remove: ")
+    configuration.remove_section(remove)
+    
+    print("Configuration successfully removed!")
+
+
+##NOT DONE YET##
 def editConfig():
-    pass
+    listConfig()
+    edit = input("Choose section to edit: ")
+    
+    configuration[edit]['turnIP'] = turnIP
+    configuration[edit]['turnPort'] = turnPort
+    configuration[edit]['protocol'] = protocol
+    
+    print("Configuration successfully edited!")
+    
+def displayConfig():
+    listConfig()
+    display = input("Choose configuration to display: ")
+    print("TURN IP: " + configuration[display]['turnIP'])
+    print("TURN Port: " + configuration[display]['turnPort'])
+    print("Protocol: " + configuration[display]['protocol'])
+
 
 def listConfig():
     print(configuration.sections())
 
-def configOptions():
-    print('1. create\n2. remove\n3. edit\n4. list\n5. exit\n')
 
-##Main Functions
+def configOptions():
+    print("> create\n> remove\n> edit\n> list\n> display\n> exit\n")
+
+
+## Main Functions ##
 def welcome():
-        try:
-            with open("w1.txt", "r") as f:
-                print(f.read())
-        except FileNotFoundError:
-            print("Error: Welcome file not found...")    
-        print("Type help to see the available commands or exit to quit...\n\n")
-    
+    try:
+        with open("w1.txt", "r") as f:
+            print(f.read())
+    except FileNotFoundError:
+        print("Error: Welcome file not found...")
+    print("Type help to see the available commands or exit to quit...\n\n")
+
+
 def trippyMako():
     while True:
         command = input("\n> ").strip()
-        
+
         match command:
-            case 'exit':
+            case "exit":
                 print("Exiting...")
-                break             
-            case 'help':
+                break
+            case "help":
                 help()
-            case 'config':
+            case "config":
                 config()
-            case 'send':
+            case "send":
                 sendPayload()
-            case 'proxy':
+            case "proxy":
                 proxy()
             case _:
                 print("Unrecognized Command...")
 
+
 ## MAIN ##
-if __name__ == '__main__':
-    #User Configurations
-    configuration = configparser.ConfigParser()
-    configuration.read('configs.ini')
-    
+if __name__ == "__main__":
     welcome()
     trippyMako()
