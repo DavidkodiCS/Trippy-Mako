79f271048389d68bc2e14d41109bf70b033b5169
m253312@usna.edu <m253312@usna.edu>
2025-02-07 20:42:23 -0500

    built out data indication but has some issues

--- Trippy-Mako/trippy-mako/core/turnTM.py
+++ Trippy-Mako/trippy-mako/core/turnTM.py
@@ -224,10 +224,34 @@ def build_send_indication(ip, port, payload):
     
     return header + xor_peer_address + data_attribute
 
-def build_data_indication():
+def build_data_indication(ip, port, payload):
+    ## XOR-Peer-Address Attribute
+    MAGIC_COOKIE = 0x2112A442
+    ## XOR PORT + MAGIC COOKIE
+    xor_port = port ^ (MAGIC_COOKIE >> 16)
+    
+    ## IP address to Bytes
+    ip_bytes = bytes([ip[i] ^ ((MAGIC_COOKIE >> (8 * (3 - i))) & 0xFF) for i in range(4)])
+    
+    MESSAGE_TYPE = 0x0012
+    
+    xor_peer_address = struct.pack("!HHBBH4s", 
+        MESSAGE_TYPE,  # Attribute Type (XOR-PEER-ADDRESS)
+        8,       # Length
+        0,       # Reserved
+        0x01,    # Family (IPv4)
+        xor_port,  # XOR'ed Port
+        ip_bytes)  # XOR'ed IP Address
+    
+    xor_message_length = len(xor_peer_address)
+    
+    ## Data Attribute
+    DATA_ATTRIBUTE_TYPE = 0x0013  # Attribute type for DATA
+    data_length = len(payload)
+    data_attribute = struct.pack("!HH", DATA_ATTRIBUTE_TYPE, data_length) + payload
+
     STUN_HEADER_FORMAT = "!HHI12s"
     MESSAGE_TYPE = 0x007
-    MAGIC_COOKIE = 0x2112A442
     TRANSACTION_ID = os.urandom(12)
     
     # Pack the header (Type, Length, Magic Cookie, Transaction ID)
@@ -239,7 +263,7 @@ def build_data_indication():
         TRANSACTION_ID          # Transaction ID
     )
     
-    return header
+    return header + xor_peer_address + data_attribute
 
 # NOT NEEDED RIGHT NOW
 # def build_channelBind():
